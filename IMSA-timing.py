abceeee41cccfb9d24d43c15bcd411327cd0cebc
uujfwmnp <3219994+uujfwmnp@users.noreply.github.com>
2019-01-26 16:58:35 -0800

    Initial commit
    Initial commit of timing script. Pulls from two feeds, RaceResults_JSONP.json and SessionInfo_JSONP.json. Still having some problems formatting driver names, but it works well enough.

---
+++ IMSA-timing.py
@@ -0,0 +1,106 @@
+import requests
+import json
+import time, os, sys
+
+def timing():
+    #Setup Timing & Scoring
+    tsGet = requests.get('https://scoring.imsa.com/scoring_data/RaceResults_JSONP.json')    # Request data from Indycar
+    rawdata = tsGet.text                                                                    # Set text of GET reply as a variable
+    top = rawdata.replace("jsonpRaceResults(", "")                                          # Remove top line that is not JSON valid
+    bottom = top.replace(");", "")                                                          # Remove bottom line that is not JSON valid
+    tsData = json.loads(bottom)                                                             # Load the formatted string as JSON
+    sessionGet = requests.get('https://scoring.imsa.com/scoring_data/SessionInfo_JSONP.json')
+    rawdata = sessionGet.text
+    top = rawdata.replace("jsonpSessionInfo(", "")
+    bottom = top.replace(");", "")
+    session = json.loads(bottom)
+
+# LOCAL DEBUG
+#    json_data = open("JSON FILE HERE", "r").read()
+#    data = json.loads(json_data)
+
+    #Setup Array Event Variables
+    global eventName, eventFlag, eventTime, eventLeft, drivers
+
+    eventName    = "Event Name:    " + session['T'] + " " + session['S']
+    eventFlag    = "Status:        " + session['F']
+    eventTime    = "Elapsed Time:  " + session['TT']
+    eventLeft    = "Time Left:     " + session['TR']
+
+    #Setup Array Driver Variables
+    drivers = tsData['B']
+
+    #Start the show!
+def lapSpacing(length):
+    switcher = {
+        5: "               ",
+        6: "              ",
+        7: "             ",
+        8: "            "
+    }
+    result = switcher.get(length, "  ")
+    return result
+def gapSpacing(length):
+    switcher = {
+        5: "        ",
+        6: "       ",
+        7: "      ",
+        8: "     ",
+        9: "  "
+    }
+    result = switcher.get(length, "  ")
+    return result
+
+def event():
+    try:
+        while True:
+            timing()
+            #Pretty Stuff Here
+            if (sys.platform == "win32"):
+                os.system('cls')
+            else:
+                os.system('clear')
+            print("!! PRESS CONTROL-C TO END THE PROGRAM !!")
+            print(eventName,"\n",
+              eventFlag,"\n",
+              eventTime,"\n",
+              eventLeft,"\n",sep='')
+
+            print ("Overall: ", "Class: ", "Driver: \t\t\t", "Num:\t", "Race Leader: ", "Class Leader:", "Gap to Class:" , "Last Lap:    ", "Car:")
+            
+            # Driver Variable Array
+            for i in range(0, len(drivers)):
+                overallPosition = drivers[i]['A']
+#                classPosition = drivers[i]['PIC']
+                carClass = drivers[i]['C']
+                nameLen = len(drivers[i]['F'])
+                if (nameLen >= 20):
+                    driverName = drivers[i]['F']+" "
+                elif (nameLen >= 16 and nameLen <= 19):
+                    driverName = drivers[i]['F']+"\t"
+                elif (nameLen == 15 or nameLen == 14):
+                    driverName = drivers[i]['F']+"\t"
+                elif (nameLen == 12 or nameLen == 13):
+                    driverName = drivers[i]['F']+"\t\t"
+                else:
+                    driverName = drivers[i]['F'] + "\t\t"
+                carNum = drivers[i]['N']
+                carManuf = drivers[i]['V']
+                bestLapTime = drivers[i]['BL']  + lapSpacing(len(drivers[i]['BL']))
+                lastLapTime = drivers[i]['LL']
+                diff2OLead  = drivers[i]['D']   + gapSpacing(len(drivers[i]['D']))
+                diff2CLead  = drivers[i]['DIC'] + gapSpacing(len(drivers[i]['DIC']))
+                classGap    = drivers[i]['GIC'] + gapSpacing(len(drivers[i]['GIC']))
+
+                print (overallPosition, "\t ", carClass, "\t ", driverName, "\t", carNum, "\t", diff2OLead, diff2CLead, classGap, lastLapTime, "\t", carManuf)
+
+            time.sleep(10)
+            print("Refreshing. . .")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Ending Program\n")
+        quit()
+    pass
+
+# Start the whole thing
+event()
